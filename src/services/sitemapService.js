9ad50113fca7e1aec8a59554266c276dfdad3ab1
leorsousa05 <leorsousa05@gmail.com>
2024-07-17 19:43:27 -0300

    feat: XML Options

--- src/services/sitemapService.js
+++ src/services/sitemapService.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
 const { readJsonFile } = require("../utils/fileReader.js");
+const { removeAccents, removeSpaces } = require("../utils/string.js");
 
 function generateSitemap(inputFilePath, outputFilePath, websiteUrl) {
 	readJsonFile(inputFilePath)
 		.then(data => {
 			const keywords = data.keywords;
-			const urls = keywords.map(keyword => websiteUrl + keyword);
+			const urls = keywords.map(keyword => {
+				const urlWithoutAccents = removeAccents(keyword);
+				const urlFormatted = removeSpaces(urlWithoutAccents).toLowerCase();
+				return websiteUrl + urlFormatted;
+			});
 			const sitemap = createSitemap(urls)
 			fs.writeFileSync(outputFilePath, sitemap);
 		})
@@ -21,7 +26,9 @@ function createSitemap(urls) {
 	urls.forEach(url => {
 		sitemap += '  <url>\n';
 		sitemap += `    <loc>${url}</loc>\n`;
-		sitemap += '    <priority>1.0</priority>\n';
+		sitemap += '    <priority>0.8</priority>\n';
+		sitemap += '    <lastmod>2024-07-04T10:32:11+00:00</lastmod>\n';
+		sitemap += '    <changefreq>weekly</changefreq>\n';
 		sitemap += '  </url>\n';
 	});
 
